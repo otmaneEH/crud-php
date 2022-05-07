1462d420ddd448ac7f85a95498f09f5cec45701f
otmanee7 <76048466+otmaneelhaddaji@users.noreply.github.com>
2022-05-07 22:56:18 +0000

    l

--- add.php
+++ add.php
@@ -65,6 +65,34 @@ if (isset($_POST['Submit'])) {
 		</table>
 	</form>
 
+
+	<form>
+  <div class="mb-3">
+  <div class="alert alert-danger" role="alert">
+				<?php if (isset($nameEr)) {
+					echo "$nameEr";
+				} ?><br>
+				<?php if (isset($emailEr)) {
+					echo "$emailEr";
+				} ?>
+				<?php if (isset($bac)) {
+					echo "$bac";
+				} ?> </div>
+    <label for="exampleInputEmail1" class="form-label">Name</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="email" class="form-label">Email</label>
+    <input type="text" class="form-control" id="email">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
 </body>
 
 </html>
