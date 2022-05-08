c4ac5b55635b8be3971c3be634f2458ea9a7741a
otmanee7 <76048466+otmaneelhaddaji@users.noreply.github.com>
2022-05-08 11:54:05 +0100

    Update header.php

--- header.php
+++ header.php
@@ -33,13 +33,7 @@
       </ul>
 
 
-      <div class="col-sm-2">
-        <div class="input-group rounded">
-          <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-          <span class="input-group-text border-0" id="search-addon">
-            <i class="fas fa-search"></i>
-          </span>
-        </div>
+     
       </div>
     </div></div>
   </nav>
