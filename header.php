db049279540abb13bb34e49b414a992bf4d257de
otmanee7 <76048466+otmaneelhaddaji@users.noreply.github.com>
2022-05-10 16:50:24 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,28 +1,34 @@
+
 <!doctype html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
+    <link rel="icon" href="img/otmane.png" type="image/icon type">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.72.0">
     <title>Product example · Bootstrap</title>
 
-    <link rel="canonical" href="https://v5.getbootstrap.com/docs/5.0/examples/product/">
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css"
+    <link rel="stylesheet" href="scripts/bootstrap.min.css"
         integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"
+    <script src="scripts/bootstrap.min.js"
         integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/"
         crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <!--Sweet Alert CDN-->
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="scripts/popper.min.js"
         integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
         crossorigin="anonymous"></script>
+        <script src="scripts/js.js" defer></script>
+        <link rel="stylesheet" href="scripts/css.css">
 
 
 
     <style>
+
         .bd-placeholder-img {
             font-size: 1.125rem;
             text-anchor: middle;
@@ -43,13 +49,11 @@
     <!-- Custom styles for this template -->
     <!-- <link href="product.css" rel="stylesheet"> -->
     <style>
-        .container {
-            max-width: 960px;
+        .hide{
+            visibility:hidden
         }
-
-        /*
- * Custom translucent site header
- */
+        .container {
+            max-width: 960px}
 
         .site-header {
             background-color: rgba(0, 0, 0, .85);
@@ -58,86 +62,29 @@
         }
 
         .site-header a {
-            color: #727272;
+            color: #fff;
             transition: color .15s ease-in-out;
         }
 
-        .site-header a:hover {
-            color: #fff;
-            text-decoration: none;
-        }
-
-        /*
- * Dummy devices (replace them with your own or something else entirely!)
- */
-
-        .product-device {
-            position: absolute;
-            right: 10%;
-            bottom: -30%;
-            width: 300px;
-            height: 540px;
-            background-color: #333;
-            border-radius: 21px;
-            transform: rotate(30deg);
-        }
-
-        .product-device::before {
-            position: absolute;
-            top: 10%;
-            right: 10px;
-            bottom: 10%;
-            left: 10px;
-            content: "";
-            background-color: rgba(255, 255, 255, .1);
-            border-radius: 5px;
-        }
-
-        .product-device-2 {
-            top: -25%;
-            right: auto;
-            bottom: 0;
-            left: 5%;
-            background-color: #e5e5e5;
-        }
-
-
-        /*
- * Extra utilities
- */
-
-        .flex-equal>* {
-            flex: 1;
-        }
-
-        @media (min-width: 768px) {
-            .flex-md-equal>* {
-                flex: 1;
-            }
-        }
-    </style>
+</style>
 </head>
 
 <body>
 
-    <nav class="site-header sticky-top py-1">
-        <div class="container d-flex flex-column flex-md-row justify-content-between">
-            <a class="py-2" href="#" aria-label="Product">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor"
-                    stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mx-auto" role="img"
-                    viewBox="0 0 24 24">
-                    <title>php control</title>
-                    <circle cx="12" cy="12" r="10" />
-                    <path
-                        d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94" />
-                </svg>
-            </a>
-            <a class="py-2 d-none d-md-inline-block" href="index.php">Home</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Add New Data</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Features</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Enterprise</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Support</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Pricing</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Cart</a>
-        </div>
-    </nav>
+<nav class="site-header sticky-top py-1">
+    <div class="container d-flex flex-column flex-md-row justify-content-between">
+        <a class="py-2" href="index.php" aria-label="Product">
+            <img src="img/home.png" alt="imagenav" style="height:25px;width:25px;">
+                <title>php control</title>
+                <circle cx="12" cy="12" r="10" />
+                <path
+                    d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94" />
+            </svg>
+        </a>
+        <a class="py-2 d-none d-md-inline-block" href="index.php">Home</a>
+        <a class="py-2 d-none d-md-inline-block" href="creer.php">Add New Data</a>
+        <!-- <a class="py-2 d-none d-md-inline-block hide">Add New Data</a>
+        <a class="py-2 d-none d-md-inline-block hide " >Add New Data</a>
+        <a class="py-2 d-none d-md-inline-block hide">Add New Data</a> -->
+    </div>
+</nav>
